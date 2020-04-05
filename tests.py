3b72025eb19ff9d827b8cf4942cf50d03bcb419e
Semen <mnogono@gmail.com>
2020-04-05 22:31:45 +0500

    removed unused code block

--- tests.py
+++ tests.py
@@ -7,10 +7,6 @@ from visiobas_property import ObjectProperty
 import visiobas_logging
 from random import randrange
 
-if __name__ == 'main':
-    print(certifi.where())
-    exit(0)
-
 LOCAL = {
     "host": "http://localhost",
     "port": 8080,
@@ -53,30 +49,12 @@ class TestVisiobasClient(unittest.TestCase):
         objects = self.client.rq_children(reference)
         self.assertTrue(len(objects) >= 0)
 
-    # def test_rise_exception_on_logout(self):
-    #     client = VisiobasClient(HOST, PORT, verify=False)
-    #     handle_value_error = False
-    #     try:
-    #         client.rq_logout()
-    #     except ValueError:
-    #         handle_value_error = True
-    #     self.assertEqual(handle_value_error, True, "Expected ValueError exception")
-    #
-    # def test_login_verify_logout(self):
-    #     client = VisiobasClient(HOST, PORT, verify=False)
-    #     client.rq_login(USER, PWD)
-    #     # self.assertTrue(client.rq_check_auth_token(), "Authorization token check false")
-    #     client.rq_logout()
-
 
 class TestVisiobasGateClient(unittest.TestCase):
     def setUp(self):
         visiobas_logging.initialize_logging()
         self.client = VisiobasGateClient(HOST, PORT, verify=False)
         self.client.rq_login(USER, PWD)
-        # if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
-        #         getattr(ssl, '_create_unverified_context', None)):
-        #     ssl._create_default_https_context = ssl._create_unverified_context
 
     def tearDown(self):
         self.client.rq_logout()
